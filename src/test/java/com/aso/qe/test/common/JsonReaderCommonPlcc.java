9a18d1d636ab1e5d73bed233d9b8e4a8c55df86d
Vidya Siddaramappa <C0015187@academy.com>
2019-02-22 11:31:47 +0530

    changes committed for API path

---
+++ src/test/java/com/aso/qe/test/common/JsonReaderCommonPlcc.java
@@ -0,0 +1,11 @@
+package com.aso.qe.test.common;
+
+import org.apache.log4j.Logger;
+
+public class JsonReaderCommonPlcc {
+	private static final Logger logger = Logger.getLogger(JsonReaderCommonPlcc.class);
+	public static final String  jsonRequestFolderPathPLCC = System.getProperty("user.dir")+ "/src/test/resources/plccApiJsonRequest/";
+	public static final String  jsonSchemaFolderPathPLCC = System.getProperty("user.dir")+ "/src/test/resources/plccApiJsonSchema/";
+	
+	
+}
--- src/test/java/com/aso/qe/test/stepdefinition/api/plcc/applyInstantCreditCard.java
+++ src/test/java/com/aso/qe/test/stepdefinition/api/plcc/applyInstantCreditCard.java
@@ -1,20 +1,15 @@
 package com.aso.qe.test.stepdefinition.api.plcc;
 
 import static org.testng.Assert.assertTrue;
-import static org.testng.Assert.assertNotNull;
 
-import java.io.IOException;
 import org.apache.log4j.Logger;
-import com.aso.qe.framework.api.helpers.JSONValidationUtils;
-import com.aso.qe.framework.api.json.JsonReaderCommon;
-import com.aso.qe.framework.common.FrameWorkHelper;
-import com.aso.qe.test.stepdefinition.api.R1SP1_Categories_ProductsByCategorySD;
-import com.github.fge.jsonschema.core.exceptions.ProcessingException;
 
-import cucumber.api.PendingException;
+import com.aso.qe.framework.api.helpers.JSONValidationUtils;
+import com.aso.qe.test.common.JsonReaderCommonPlcc;
+import com.aso.qe.test.stepdefinition.api.R1SP1_Categories_ProductsByCategorySD;
+
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
-import io.restassured.path.json.JsonPath;
 
 public class applyInstantCreditCard extends JSONValidationUtils {
 	private static final Logger logger = Logger.getLogger(R1SP1_Categories_ProductsByCategorySD.class);
@@ -24,24 +19,24 @@ public class applyInstantCreditCard extends JSONValidationUtils {
 	public void endpoint_with_for_applying_instant_credit_card(String PLCCApplyInstantCardURLr2, String PLCCApplyInstantCardPostRequest) throws Throwable {
 		String endpoints=apiEndpointIP+loadProps.getTestDataProperty(PLCCApplyInstantCardURLr2);
 		logger.debug("END Point URL:"+endpoints);
-		String postRequestStr = JSONValidationUtils.convertJsonFileToString(JsonReaderCommon.jsonRequestFolderPathPLCC+ loadProps.getTestDataProperty(PLCCApplyInstantCardPostRequest)+".json");
-		logger.info(JsonReaderCommon.jsonRequestFolderPathPLCC+ loadProps.getTestDataProperty(PLCCApplyInstantCardPostRequest)+".json");
+		String postRequestStr = JSONValidationUtils.convertJsonFileToString(JsonReaderCommonPlcc.jsonRequestFolderPathPLCC+ loadProps.getTestDataProperty(PLCCApplyInstantCardPostRequest)+".json");
+		logger.info(JsonReaderCommonPlcc.jsonRequestFolderPathPLCC+ loadProps.getTestDataProperty(PLCCApplyInstantCardPostRequest)+".json");
 	}
 	
 	@Given("^\"(.*?)\" endpoint with \"(.*?)\" for applying invalid instant credit card$")
 	public void endpoint_with_for_applying_invalid_instant_credit_card(String PLCCApplyMissingZipCodeInstantCardURLr2, String PLCCApplyMissingZipCodeInstantCardPostRequest) throws Throwable {
 		String endpoints=apiEndpointIP+loadProps.getTestDataProperty(PLCCApplyMissingZipCodeInstantCardURLr2);
 		logger.debug("END Point URL:"+endpoints);
-		String postRequestStr = JSONValidationUtils.convertJsonFileToString(JsonReaderCommon.jsonRequestFolderPathPLCC+ loadProps.getTestDataProperty(PLCCApplyMissingZipCodeInstantCardPostRequest)+".json");
-		logger.info(JsonReaderCommon.jsonRequestFolderPathPLCC+ loadProps.getTestDataProperty(PLCCApplyMissingZipCodeInstantCardPostRequest)+".json");
+		String postRequestStr = JSONValidationUtils.convertJsonFileToString(JsonReaderCommonPlcc.jsonRequestFolderPathPLCC+ loadProps.getTestDataProperty(PLCCApplyMissingZipCodeInstantCardPostRequest)+".json");
+		logger.info(JsonReaderCommonPlcc.jsonRequestFolderPathPLCC+ loadProps.getTestDataProperty(PLCCApplyMissingZipCodeInstantCardPostRequest)+".json");
 	}
 	
 	@Given("^\"(.*?)\" endpoint with \"(.*?)\" Not found error for applying invalid instant credit card$")
 	public void endpoint_with_Not_found_error_for_applying_invalid_instant_credit_card(String PLCCApplyInvalidInstantCardInvalidURL, String PLCCApplyInvalidInstantCardPostRequest) throws Throwable {
 		String endpoints=apiEndpointIP+loadProps.getTestDataProperty(PLCCApplyInvalidInstantCardInvalidURL);
 		logger.debug("END Point URL:"+endpoints);
-		String postRequestStr = JSONValidationUtils.convertJsonFileToString(JsonReaderCommon.jsonRequestFolderPathPLCC+ loadProps.getTestDataProperty(PLCCApplyInvalidInstantCardPostRequest)+".json");
-		logger.info(JsonReaderCommon.jsonRequestFolderPathPLCC+ loadProps.getTestDataProperty(PLCCApplyInvalidInstantCardPostRequest)+".json");
+		String postRequestStr = JSONValidationUtils.convertJsonFileToString(JsonReaderCommonPlcc.jsonRequestFolderPathPLCC+ loadProps.getTestDataProperty(PLCCApplyInvalidInstantCardPostRequest)+".json");
+		logger.info(JsonReaderCommonPlcc.jsonRequestFolderPathPLCC+ loadProps.getTestDataProperty(PLCCApplyInvalidInstantCardPostRequest)+".json");
 	}
 	
 	@Then("^user get the response status code as (\\d+)$")
