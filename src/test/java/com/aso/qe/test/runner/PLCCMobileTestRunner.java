d4ee65acca203530ffc0a984606362db150ce05a
Vidya Siddaramappa <C0015187@academy.com>
2019-03-12 10:23:22 +0530

    resolving conflicts

--- src/test/java/com/aso/qe/test/runner/PLCCMobileTestRunner.java
+++ src/test/java/com/aso/qe/test/runner/PLCCMobileTestRunner.java
@@ -7,7 +7,7 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 
-@CucumberOptions(features = "src/test/resources/features/plccMobile",
+@CucumberOptions(features = "src/test/resources/features/plccMobile/R1_Checkout_81_Mobile.feature",
 glue = "com.aso.qe.test.stepdefinition.web.plcc", monochrome = true, 
 plugin = "com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/cucumber-html-reports/Report/report1.html", format = {
 		"pretty", "html:target/cucumber-reports/cucumber-html-reports",
