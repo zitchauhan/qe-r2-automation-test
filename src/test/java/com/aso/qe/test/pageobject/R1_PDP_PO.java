5cb72d0f16a8f4d436ee72a566b15389af52976b
c0015676 <c0015676@VDI-ESQA0028E.academy.com>
2019-07-17 17:48:33 +0530

    Fixes for P2

--- src/test/java/com/aso/qe/test/pageobject/R1_PDP_PO.java
+++ src/test/java/com/aso/qe/test/pageobject/R1_PDP_PO.java
@@ -32,7 +32,7 @@ public class R1_PDP_PO extends CommonActionHelper
 	
 	@FindBy(xpath = "//*[@data-auid='btnShipIt']|//*[text()='Ship It']|//*[starts-with(@data-auid,'btnShipIt')]")
 	public WebElement shipItButton; 
-
+	@FindBy(xpath="//*[@data-auid='btnPickUpATC']")public WebElement pickupBtn; //CR MJR-17/7/19
 	@FindBy(xpath="//*[@data-auid='btnAddToCart']") public WebElement AddToCartBtn;
 	@FindBy(xpath="//*[@data-auid='btnbtnEnableBuyNow']")  public WebElement btnEnablebuynow;
 	
