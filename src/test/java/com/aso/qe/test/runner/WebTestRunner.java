2fbd6e7e305d9130f5e894c9ead05cd92cf25679
hpinglia <harvindar.pinglia@academy.com>
2019-07-19 16:02:08 -0500

    Commented out Run tags

--- src/test/java/com/aso/qe/test/runner/WebTestRunner.java
+++ src/test/java/com/aso/qe/test/runner/WebTestRunner.java
@@ -22,7 +22,7 @@ import cucumber.api.junit.Cucumber;
 @CucumberOptions(
 		features="src/test/resources/features/web/", //R1_HP_KER_1915_Web.feature",//R1_PDP_KER_2334_Web.feature",
 		glue="com.aso.qe.test.stepdefinition.web",
-		tags= {"@KER-2871"},
+		//tags= {"@KER-2871"},
 		monochrome=true,
 				plugin = "com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/cucumber-html-reports/Report/report1.html",
 		format={"pretty","html:target/cucumber-reports/cucumber-html-reports", "json:target/cucumber-reports/cucumber-html-reports/common.json"}
