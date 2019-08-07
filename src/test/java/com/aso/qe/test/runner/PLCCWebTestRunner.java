8661936c94a8fc67519104896f9792d9d4903423
Rahul Singh <C0016097@academy.com>
2019-08-07 18:28:47 +0530

    adding solution for PLCC banner and card details

--- src/test/java/com/aso/qe/test/runner/PLCCWebTestRunner.java
+++ src/test/java/com/aso/qe/test/runner/PLCCWebTestRunner.java
@@ -7,10 +7,10 @@
 //
 //@RunWith(Cucumber.class)
 //
-//@CucumberOptions(features = "src/test/resources/features/web/R1_PDP_KER_2334_Web.feature",
+//@CucumberOptions(features = "src/test/resources/features/web/plcc/R1_CCAM_Generic_CartPage_112_Web.feature",
 //		glue = "com.aso.qe.test.stepdefinition.web", 
 //		monochrome = true,
-//		//tags = "@Rahul",
+//		tags = "@Rahul",
 //		plugin = "com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/cucumber-html-reports/Report/report1.html", 
 //		format = {"pretty", "html:target/cucumber-reports/cucumber-html-reports",
 //        "json:target/cucumber-reports/cucumber-html-reports/common.json" })
