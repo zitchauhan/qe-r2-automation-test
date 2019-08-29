162cd95fce2f2d792658ddeeec638c10ef173355
Navya Narkesdamilli <C0015640@academy.com>
2019-08-29 18:04:11 +0530

    just commented runner file

--- src/test/java/com/aso/qe/test/runner/PLCCWebTestRunner.java
+++ src/test/java/com/aso/qe/test/runner/PLCCWebTestRunner.java
@@ -1,21 +1,21 @@
-
-package com.aso.qe.test.runner;
-
-import org.junit.runner.RunWith;
-
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
-
-@RunWith(Cucumber.class)
-
-@CucumberOptions(features = "src/test/resources/features/web/SiteHealth/ES199.feature",
-		glue = "com.aso.qe.test.stepdefinition.web", 
-		monochrome = true,
-		format={"pretty","html:target/cucumber-reports/cucumber-html-reports", "json:target/cucumber-reports/cucumber-html-reports/common.json"}
-		) 
-
-
-public class PLCCWebTestRunner {
-}
-
-
+//
+//package com.aso.qe.test.runner;
+//
+//import org.junit.runner.RunWith;
+//
+//import cucumber.api.CucumberOptions;
+//import cucumber.api.junit.Cucumber;
+//
+//@RunWith(Cucumber.class)
+//
+//@CucumberOptions(features = "src/test/resources/features/web/SiteHealth/ES199.feature",
+//		glue = "com.aso.qe.test.stepdefinition.web", 
+//		monochrome = true,
+//		format={"pretty","html:target/cucumber-reports/cucumber-html-reports", "json:target/cucumber-reports/cucumber-html-reports/common.json"}
+//		) 
+//
+//
+//public class PLCCWebTestRunner {
+//}
+//
+//
