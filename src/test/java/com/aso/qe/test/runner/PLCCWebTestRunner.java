2243d7af811e1c8ba096c2434c0f742dbe32ec1a
Rahul Singh <C0016097@academy.com>
2019-09-04 18:40:37 +0530

    removing plcc conflicts

--- src/test/java/com/aso/qe/test/runner/PLCCWebTestRunner.java
+++ src/test/java/com/aso/qe/test/runner/PLCCWebTestRunner.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 //
 //package com.aso.qe.test.runner;
 //
@@ -23,26 +23,3 @@
 //
 //
 //	
-=======
-/*
-package com.aso.qe.test.runner;
-
-import org.junit.runner.RunWith;
-
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
-
-@RunWith(Cucumber.class)
-
-@CucumberOptions(features = "src/test/resources/features/web/SiteHealth/ES199.feature",
-		glue = "com.aso.qe.test.stepdefinition.web", 
-		monochrome = true,
-		format={"pretty","html:target/cucumber-reports/cucumber-html-reports", "json:target/cucumber-reports/cucumber-html-reports/common.json"}
-		) 
-
-
-public class PLCCWebTestRunner {
-}
-
-*/
->>>>>>> 1ec8f2925f3b6687c0f23a813ae038a4d4254a15
