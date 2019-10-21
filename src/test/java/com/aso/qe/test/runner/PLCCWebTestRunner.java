962209bbc7152f1dfc7f33e2198a4475dc7776b2
Navya Narkesdamilli <C0015640@academy.com>
2019-10-21 17:27:56 +0530

    just commented plcc webrunner

--- src/test/java/com/aso/qe/test/runner/PLCCWebTestRunner.java
+++ src/test/java/com/aso/qe/test/runner/PLCCWebTestRunner.java
@@ -1,22 +1,22 @@
-
-
-package com.aso.qe.test.runner;
-
-import org.junit.runner.RunWith;
-
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
-
-@RunWith(Cucumber.class)
-
-@CucumberOptions(features="src/test/resources/features/web/R2_HP_KER_15867.feature", //R1_HP_KER_1915_Web.feature",//R1_PDP_KER_2334_Web.feature",
-glue="com.aso.qe.test.stepdefinition.web",
-tags= {"@Navya"},
-monochrome=true,
-plugin = "com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/cucumber-html-reports/Report/report1.html",
-format={"pretty","html:target/cucumber-reports/cucumber-html-reports", "json:target/cucumber-reports/cucumber-html-reports/common.json"})
-
-
-public class PLCCWebTestRunner {
-}
-	
+//
+//
+//package com.aso.qe.test.runner;
+//
+//import org.junit.runner.RunWith;
+//
+//import cucumber.api.CucumberOptions;
+//import cucumber.api.junit.Cucumber;
+//
+//@RunWith(Cucumber.class)
+//
+//@CucumberOptions(features="src/test/resources/features/web/R2_HP_KER_15867.feature", //R1_HP_KER_1915_Web.feature",//R1_PDP_KER_2334_Web.feature",
+//glue="com.aso.qe.test.stepdefinition.web",
+//tags= {"@Navya"},
+//monochrome=true,
+//plugin = "com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/cucumber-html-reports/Report/report1.html",
+//format={"pretty","html:target/cucumber-reports/cucumber-html-reports", "json:target/cucumber-reports/cucumber-html-reports/common.json"})
+//
+//
+//public class PLCCWebTestRunner {
+//}
+//	
