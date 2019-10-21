8f605c2f19ba5a13901d3d3d66dae1d4f5118bb0
c0015676 <c0015676@VDI-ESQA0028E.academy.com>
2019-10-21 16:27:04 +0530

    .

--- src/test/java/com/aso/qe/test/runner/WebTestRunner.java
+++ src/test/java/com/aso/qe/test/runner/WebTestRunner.java
@@ -1,5 +1,4 @@
 package com.aso.qe.test.runner;
-
 import org.junit.runner.RunWith;
 
 import cucumber.api.CucumberOptions;
@@ -23,8 +22,8 @@ import cucumber.api.junit.Cucumber;
 		features="src/test/resources/features/web/", //R1_HP_KER_1915_Web.feature",//R1_PDP_KER_2334_Web.feature",
 		glue="com.aso.qe.test.stepdefinition.web",
 		//tags= {"@Regression", "~@ignoreUAT7 "},
-		monochrome=true,
-		plugin = "com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/cucumber-html-reports/Report/report1.html",
+		monochrome=true, 
+				plugin = "com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/cucumber-html-reports/Report/report1.html",
 		format={"pretty","html:target/cucumber-reports/cucumber-html-reports", "json:target/cucumber-reports/cucumber-html-reports/common.json"}
 		) 
 public class WebTestRunner{	
