991d15fdf865d0c2c0bb945aec78adddf6152957
Sachin Bisht <sacbisht@publicisgroupe.net>
2020-04-15 15:22:50 +0530

    fixes for shipit cta

--- src/test/java/com/aso/qe/test/stepdefinition/web/plcc/R1_Checkout_Discounts_102_Web.java
+++ src/test/java/com/aso/qe/test/stepdefinition/web/plcc/R1_Checkout_Discounts_102_Web.java
@@ -4,9 +4,11 @@ import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.xbill.DNS.ISDNRecord;
 
 import com.aso.qe.framework.common.CommonActionHelper;
 import com.aso.qe.framework.common.Constants;
@@ -31,12 +33,41 @@ public class R1_Checkout_Discounts_102_Web extends CommonActionHelper {
 		//clickOnButton(genericPO.shipItButton);
 		
 		//changes done in order resolve failure of uat6 automation running on gcp -- Sachin
-		((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", genericPO.shipItButton);
+		System.out.println("size" + driver.findElements(By.xpath("//div[@class='large']//*[text()='Home delivery'] | (//div[text()='Home delivery'])[3] | (//div[contains(text(),'Ship to Store')])[1]")).size());
+		
+		scrollWithLoop(driver, 0, 100, By.xpath("//div[@class='large']//*[text()='Home delivery'] | (//div[text()='Home delivery'])[3] | (//div[contains(text(),'Ship to Store')])[1]")); 
+		
+		//((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", genericPO.shipItButton);
 		Thread.sleep(Constants.thread_high);
 		((JavascriptExecutor) driver).executeScript("arguments[0].click();", genericPO.shipItButton);
 		Thread.sleep(Constants.thread_high);
 		
 
 	}
-	
+
+	public static void scrollWithLoop(WebDriver driver, int x, int y, By shipItButton) throws InterruptedException {
+		int scrollCounter = 1;
+		while (scrollCounter <= 20) {
+		    JavascriptExecutor executor = (JavascriptExecutor) driver;
+		    executor.executeScript("window.scroll(" + x + "," + y * scrollCounter + ");");
+		    Thread.sleep(1000);
+		    scrollCounter++;
+		    if (isElementPresent(driver, shipItButton)) {
+			System.out.println("Scrolled to: x = " + x + " y = " + y * scrollCounter);
+			break;
+		    }
 		}
+	    } 
+	
+
+	public static boolean isElementPresent(WebDriver driver, By shipItButton) {
+	try {
+	    driver.findElement(shipItButton);
+	    Thread.sleep(Constants.thread_low);
+	    return true;
+	} catch (Exception e) {
+	    System.err.println(e.getMessage());
+	    return false;
+	}
+    }
+}
