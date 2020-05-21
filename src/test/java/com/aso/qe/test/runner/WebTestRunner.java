1dca5cff1543e0365d21057c7c448747e0db473f
anirudh_mahuli <anirudh.mahuli@academy.com>
2020-05-21 18:39:47 +0530

    UAT7 script fix

--- src/test/java/com/aso/qe/test/runner/WebTestRunner.java
+++ src/test/java/com/aso/qe/test/runner/WebTestRunner.java
@@ -21,7 +21,7 @@ import cucumber.api.junit.Cucumber;
 @CucumberOptions(
 		features="src/test/resources/features/web/", 
 		glue="com.aso.qe.test.stepdefinition.web",
-		//tags= {"@KER-2871"},
+		//tags= {"@unq"},
 		monochrome=true,
 				plugin = "com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/cucumber-html-reports/Report/report1.html",
 		format={"pretty","html:target/cucumber-reports/cucumber-html-reports", "json:target/cucumber-reports/cucumber-html-reports/common.json"}
