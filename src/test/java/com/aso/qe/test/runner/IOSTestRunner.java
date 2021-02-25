4270f958c85aef7fe1d0fe84331899cefaaa455f
Jitendra Singh <jitendra.singh1@publicissapient.com>
2021-02-25 20:46:13 +0530

    modify config details for first time run

--- src/test/java/com/aso/qe/test/runner/IOSTestRunner.java
+++ src/test/java/com/aso/qe/test/runner/IOSTestRunner.java
@@ -11,7 +11,7 @@ import cucumber.api.junit.Cucumber;
 @CucumberOptions(
 		features="src/test/resources/features/ios/",
 		glue="com.aso.qe.test.stepdefinition.ios",
-		tags= {"@TC_OMNI_24466"},
+		tags= {"@TC-OMNI-24466"},
 		monochrome=true, 
 				
 //		plugin = {"pretty",
