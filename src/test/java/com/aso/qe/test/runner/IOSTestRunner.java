e553652e7ed5615f9d2175803bffeb0eace19e08
Jitendra Singh <jitendra.singh1@publicissapient.com>
2021-03-13 01:06:27 +0530

    update configs for retesting

--- src/test/java/com/aso/qe/test/runner/IOSTestRunner.java
+++ src/test/java/com/aso/qe/test/runner/IOSTestRunner.java
@@ -11,7 +11,7 @@ import cucumber.api.junit.Cucumber;
 @CucumberOptions(
 		features="src/test/resources/features/ios/",
 		glue="com.aso.qe.test.stepdefinition.ios",
-		tags= {"@TC-OMNI-20609"},
+		tags= {"@TC-OMNI-20656"},
 		monochrome=true,
 		//dryRun=true,
 				
