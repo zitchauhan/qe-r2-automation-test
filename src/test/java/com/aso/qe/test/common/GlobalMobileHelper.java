7e5aaa4802cb2fe1db651f81adc262f27e7f573f
Jitendra Singh <jitendra.singh1@publicissapient.com>
2021-03-12 21:19:06 +0530

    add swipe to GlobalMobileHelper

--- src/test/java/com/aso/qe/test/common/GlobalMobileHelper.java
+++ src/test/java/com/aso/qe/test/common/GlobalMobileHelper.java
@@ -3,8 +3,10 @@ package com.aso.qe.test.common;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
+import java.time.Duration;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -13,10 +15,13 @@ import com.aso.qe.framework.common.PropertiesHelper;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.ios.IOSDriver;
 import io.appium.java_client.remote.HideKeyboardStrategy;
 import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.touch.WaitOptions;
+import io.appium.java_client.touch.offset.PointOption;
 
 public class GlobalMobileHelper {
 	
@@ -109,8 +114,73 @@ public class GlobalMobileHelper {
 		tapOnElement(tapLocatorToHideKeyboard);
 	}
 	
-	 public static void setImplicitWaitTo(AppiumDriver<MobileElement> driver, int seconds) {
+	public static void setImplicitWaitTo(AppiumDriver<MobileElement> driver, int seconds) {
 			driver.manage().timeouts().implicitlyWait(seconds, TimeUnit.SECONDS);
-		    }
+	}
 	 
+	public void swipeScreen(Direction dir) {
+        System.out.println("swipeScreenSmall(): dir: '" + dir + "'"); // always log your actions
+
+	// Animation default time:
+	//  - Android: 300 ms
+	//  - iOS: 200 ms
+	// final value depends on your app and could be greater
+	final int ANIMATION_TIME = 1500; // ms
+	
+	final int PRESS_TIME = 200; // ms
+	
+	PointOption pointOptionStart, pointOptionEnd;
+	
+	// init screen variables
+	Dimension dims = driver.manage().window().getSize();
+	
+	// init start point = center of screen
+	pointOptionStart = PointOption.point(dims.width / 2, dims.height / 2);
+	
+	// reduce swipe move into multiplier times comparing to swipeScreen move
+	int mult = 5; // multiplier
+	switch (dir) {
+	case DOWN: // center of footer
+	    pointOptionEnd = PointOption.point(dims.width / 2, (dims.height / 2) + (dims.height / 2) / mult);
+	    break;
+	case UP: // center of header
+	    pointOptionEnd = PointOption.point(dims.width / 2, (dims.height / 2) - (dims.height / 2) / mult);
+	    break;
+	case LEFT: // center of left side
+	    pointOptionEnd = PointOption.point((dims.width / 2) - (dims.width / 2) / mult, dims.height / 2);
+	    break;
+	case RIGHT: // center of right side
+	    pointOptionEnd = PointOption.point((dims.width / 2) + (dims.width / 2) / mult, dims.height / 2);
+	    break;
+	default:
+	    throw new IllegalArgumentException("swipeScreenSmall(): dir: '" + dir.toString() + "' NOT supported");
+	}
+
+	// execute swipe using TouchAction
+	try {
+	new TouchAction(driver)
+	        .press(pointOptionStart)
+	        // a bit more reliable when we add small wait
+	        .waitAction(WaitOptions.waitOptions(Duration.ofMillis(PRESS_TIME)))
+	        .moveTo(pointOptionEnd)
+	        .release().perform();
+	} catch (Exception e) {
+	System.err.println("swipeScreenSmall(): TouchAction FAILED\n" + e.getMessage());
+	return;
+	}
+
+	// always allow swipe action to complete
+	try {
+	Thread.sleep(ANIMATION_TIME);
+	} catch (InterruptedException e) {
+	// ignore
+	}
+}
+
+	 public enum Direction {
+         UP,
+         DOWN,
+         LEFT,
+         RIGHT;
+     }
 }
