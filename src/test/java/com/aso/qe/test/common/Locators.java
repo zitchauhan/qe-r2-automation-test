4ddae4c3717ec9c2479ccccf75b19e670ccc6981
Jitendra Singh <jitendra.singh@academy.com>
2021-03-20 19:31:32 +0530

    complete scripting and execution of OMNI-20817

--- src/test/java/com/aso/qe/test/common/Locators.java
+++ src/test/java/com/aso/qe/test/common/Locators.java
@@ -173,4 +173,12 @@ public class Locators {
 		public static By productPrice = By.id("lbl_product_price");
 		public static By buttonCheckout = By.name("Checkout");
 	}
+	
+	public static class CheckoutPage{
+		public static By productThumbNail = By.xpath("(//XCUIElementTypeOther[@name=\"Product Image\"])[2]/XCUIElementTypeOther");
+		public static By productTitle = By.id("lbl_product_title");
+		public static By productSKU= By.xpath("//*[contains(@name,'S K U')]");
+		public static By productPrice = By.id("lbl_product_price");
+		public static By buttonCheckout = By.name("Checkout");
+	}
 }
