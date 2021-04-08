15155b89272945232f4cb0c1762f4b8f6a9efc0b
Jitendra Singh <jitendra.singh@academy.com>
2021-04-09 00:06:39 +0530

    add multiple tags and extent reports to IOSTestRunner.java
    add ExtentRunner formatter to IOSTestRunner.java

--- src/test/java/com/aso/qe/test/runner/IOSTestRunner.java
+++ src/test/java/com/aso/qe/test/runner/IOSTestRunner.java
@@ -11,12 +11,12 @@ import cucumber.api.junit.Cucumber;
 @CucumberOptions(
 		features="src/test/resources/features/ios/",
 		glue="com.aso.qe.test.stepdefinition.ios",
-		tags= {"@TC-OMNI-29800"},
+		tags= {"@TC-OMNI-29800,@TC-OMNI-30525,@TC-OMNI-30524,@ST-OMNI-26472"},
 		monochrome=true,
 		//dryRun=true,
 		plugin = "com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/cucumber-html-reports/Report/report1.html",
 		format={"pretty","html:target/cucumber-reports/cucumber-html-reports", "json:target/cucumber-reports/cucumber-html-reports/common.json"}
-)
+		)
 
 public class IOSTestRunner {
 	
