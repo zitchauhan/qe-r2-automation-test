bb2e1a7857e55553fe1214c2a623a09bae93492c
Jitendra Singh <jitendra.singh@academy.com>
2021-04-10 19:47:59 +0530

    complete script for OMNI-27857 - Edit or Remove Address

---
+++ src/test/java/com/aso/qe/test/pageobject/ios/UpdateAddressPage.java
@@ -0,0 +1,22 @@
+package com.aso.qe.test.pageobject.ios;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.support.PageFactory;
+
+public class UpdateAddressPage {
+    /*
+    Created by jitsingh7 on 10/04/21
+    */
+    private static final Logger logger = Logger.getLogger(CartPage.class.getName());
+    private AppiumDriver<MobileElement> driver;
+
+    public UpdateAddressPage(AppiumDriver<MobileElement> driver) {
+        this.driver = driver;
+        PageFactory.initElements(new AppiumFieldDecorator(driver), this);
+    }
+
+
+}
