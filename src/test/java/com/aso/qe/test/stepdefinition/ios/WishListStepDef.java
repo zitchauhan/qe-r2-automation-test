eb730ec7c77e037c545a8f232b2c2e36b5afee0b
JahnaviT-hub <Jahnavi.t@academy.com>
2021-04-26 12:05:01 +0530

    added changes

--- src/test/java/com/aso/qe/test/stepdefinition/ios/WishListStepDef.java
+++ src/test/java/com/aso/qe/test/stepdefinition/ios/WishListStepDef.java
@@ -25,7 +25,7 @@ public class WishListStepDef extends GlobalMobileHelper{
 
 	//OMNI-28640
 	@When("^User taps on \"([^\"]*)\" button$")
-	public void user_taps_on_edit_button(String arg1){
+	public void user_taps_on_button(String arg1){
 			wishlistPage.tapOnButton(arg1);
 	}
 
