2b1f0d9acdf702f4f113a0e842adb2e1003c124b
Jitendra Singh <jitendra.singh@academy.com>
2021-05-05 23:25:12 +0530

    remove mandatory capability of dismissing alerts in IOSDriver

--- src/test/java/com/aso/qe/test/common/GlobalMobileHelper.java
+++ src/test/java/com/aso/qe/test/common/GlobalMobileHelper.java
@@ -49,8 +49,6 @@ public class GlobalMobileHelper {
 		caps.setCapability(MobileCapabilityType.PLATFORM_VERSION,platformVersion);
 		caps.setCapability(MobileCapabilityType.DEVICE_NAME,deviceName);
 		caps.setCapability(MobileCapabilityType.APP, app);
-		caps.setCapability("autoDismissAlerts", true);
-
 
 	    if(platform.equalsIgnoreCase("iOS")) {
 	    	driver = new IOSDriver<MobileElement>(new URL(url),caps);
@@ -448,6 +446,7 @@ public class GlobalMobileHelper {
 		tapOnElement(Locators.SearchPage.goBtn);
 		waitForDefaultTime();
 	}
+
 }
 
 
